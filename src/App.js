fc60b35eca3d0647b7a66c1554f4904fec986361
El-Patron-01 <58299223+El-Patron-01@users.noreply.github.com>
2022-06-29 18:15:08 +0300

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { Component } from 'react';
+
+import GoodsList from './GoodsList/GoodsList';
+import GoodsListForm from './GoodsListForm/GoodsListForm';
+import { goods } from './Mocks/goodsMock';
+import {
+  changingState,
+  editingItem,
+  editingSelected,
+  getSubTotal,
+  getTotal,
+  onSaveItem,
+  removeElementById,
+} from './Mocks/utils';
+
+export default class App extends Component {
+  
+  state = {
+    goods,
+    total: getTotal(goods),
+    subtotal: getSubTotal(goods)
+  };
+
+
+  onAdd = (newElement) => {
+    const newArr = changingState(newElement, this.state.goods)
+    this.setState({
+      goods: newArr,
+      total: getTotal(newArr)
+    })
+    
+  }
+
+  deleteStateElement = (id) => {
+    const newArr = removeElementById(id, this.state.goods)
+    this.setState({
+      goods: newArr,
+      total: getTotal(newArr)
+    })
+  }
+
+  recalculateSubtotal = (id) => {
+    const newArr = editingSelected(id, this.state.goods);
+    this.setState({
+      goods: newArr,
+      subtotal: getSubTotal(newArr)
+    })
+  }
+
+  letsEditItem = (id) => {
+    editingItem(id, this.state.goods)
+    console.log(this.state.goods)
+  }
+
+  onSave = (id, obj) => {
+    onSaveItem(id, this.state.goods, obj)
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className='title'>Fridge</div>
+        <GoodsList 
+          dataSource={this.state.goods} 
+          deleteStateElement={this.deleteStateElement}
+          recalculateSubtotal={this.recalculateSubtotal}
+          editingItem={this.letsEditItem}
+          onSave={this.onSave}
+          />
+        <div className='total'>
+          <div>Total:</div>
+          <div>{ this.state.total }</div>
+        </div><div className='total'>
+          <div>Subotal:</div>
+          <div>{ this.state.subtotal }</div>
+        </div>
+        <GoodsListForm onAdd={this.onAdd}/>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
